bf5d63cfcdd27d124b335b9b53cc8ba0a65d15a9
fantom4545 <166008514+fantom4545@users.noreply.github.com>
2024-04-14 10:40:33 +0700

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+immutable_var= 12 , 34 , 'anton' ,'class'
+print(immutable_var)
+#immutable_var[1]=0
+#immutable_var.append(True) это кортеж если я бы хотел заменить элемент то использовал список
+print(immutable_var)
+mutable_list=[True , 25 , 'anton ','37 ']
+print(mutable_list)
+mutable_list[2]='artem'
+print(mutable_list)
